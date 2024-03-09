318f80799d6b49d92de3bdf577e40a62fab32ee8
abuindario <darioabuinpose@gmail.com>
2024-03-09 14:17:05 +0100

    Added functionality to homepage Search button

--- frontend_angular_17_no_standalone/src/app/app.component.ts
+++ frontend_angular_17_no_standalone/src/app/app.component.ts
@@ -95,5 +95,20 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public searchEmployees(key: String): void {
+    const results: Employee[] = [];
+    for(const employee of this.employees) {
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) != -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) != -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) != -1 
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) != -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
 }
 
